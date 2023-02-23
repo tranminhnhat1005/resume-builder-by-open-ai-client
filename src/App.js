7d1d852f044bb1a125fb59dc062866a420eea4ac
Nhat Tran <tranminhnhat1005@gmail.com>
2023-02-23 14:34:57 +0700

    master - init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Resume from "./components/Resume";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [result, setResult] = useState({});
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home setResult={setResult} />} />
+                    <Route path='/resume' element={<Resume result={result} />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
